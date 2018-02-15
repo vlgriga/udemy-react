036435cc3f959e506a55bee02a463dea325afe77
vlgriga <vladislavgriga@gmail.com>
2018-02-15 11:38:45 +0300

    Install Mongoose and MondoDB

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
 
-app.get('' , (req,res) => {
-	res.send("Vladislav besr React & Node programmer");
-});
+const mongoose = require('mongoose'); 
+const authRoutes = require('./routes/authRoutes.js');
+const keys = require('./config/keys.js');
+
+
+
+require('./services/passport');
+require('./models/user.js');
+// Mongo connect
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+authRoutes(app); 
 
 app.set('PORT', process.env.PORT || 3000);
-
 app.listen(app.get('PORT') , ()=> {
 	console.log("Server is running on port 3000");
 });
