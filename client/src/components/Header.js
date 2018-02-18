70bb88d9db111913c0c192ebf8cee4e8403f62b6
vlgriga <vladislavgriga@gmail.com>
2018-02-18 17:38:37 +0300

    Add MaterialCSS and axios+redux-thunk for ajax requests

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,14 @@ import React , { Component } from 'react';
 
 export default class Header extends Component {
     render() {
-        return(<h2>Header</h2>);
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">Emaily</a>
+                    <ul className="right">
+                        <li><a>Login With Google</a></li>
+                    </ul>
+                </div>   
+            </nav>);
     }
 }
