1d78e934ea5d85f2f5e40117f0ab161a162276ea
vlgriga <vladislavgriga@gmail.com>
2018-02-18 13:31:15 +0300

    Add redux and routes to the client side

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {Provider} from 'react-redux';
+import { createStore , applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+
+const store = createStore( reducers , {} , applyMiddleware() );
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
