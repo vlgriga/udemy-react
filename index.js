25cfbe8106787e806691b476953ef1184e533388
vlgriga <vladislavgriga@gmail.com>
2018-02-27 23:21:44 +0300

    Final auth

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const cookieSession = require('cookie-session');
 const passport = require('passport'); 
 const keys = require('./config/keys.js');
 
+const bodyParser = require('body-parser');
+
 // Mongo connect
-require('./models/user.js');
+require('./models/User.js');
+require('./models/Surveys.js');
 require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
@@ -15,6 +18,7 @@ mongoose.connect(keys.mongoURI);
 // Install App and Routes
 const app = express();
 
+app.use(bodyParser.json()); 
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -27,6 +31,15 @@ app.use(passport.session());
 
 authRoutes(app);
 billingRoutes(app);
+
+if( process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'));
+	})
+}
 
 
 app.set('PORT', process.env.PORT || 3000);
